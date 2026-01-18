7160b99901ff7e855cf8647ed40a91065ddf8350
CaioSNery <caionery40@gmail.com>
2026-01-18 12:36:48 -0300

    create:data/horaLogo

--- ViewModels/MainViewModels.cs
+++ ViewModels/MainViewModels.cs
@@ -1,14 +1,18 @@
+using System;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Windows.Input;
+using System.Windows.Threading;
 using MecAppIN.Commands;
 using MecAppIN.Models;
-
 
 namespace MecAppIN.ViewModels
 {
     public class MainViewModel : INotifyPropertyChanged
     {
+        // ===============================
+        // NAVEGAÇÃO DE TELAS
+        // ===============================
         private object _telaAtual;
         public object TelaAtual
         {
@@ -25,9 +29,28 @@ namespace MecAppIN.ViewModels
         public ICommand AbrirOrdemServicoCommand { get; }
         public ICommand AbrirBuscarOrcamentosCommand { get; }
 
+        // ===============================
+        // DATA / HORA
+        // ===============================
+        private string _dataHoraAtual;
+        public string DataHoraAtual
+        {
+            get => _dataHoraAtual;
+            set
+            {
+                _dataHoraAtual = value;
+                OnPropertyChanged();
+            }
+        }
 
+        private readonly DispatcherTimer _timer;
+
+        // ===============================
+        // CONSTRUTOR ÚNICO
+        // ===============================
         public MainViewModel()
         {
+            // Commands de navegação
             AbrirClientesCommand = new RelayCommand(() =>
                 TelaAtual = new ClientesViewModel());
 
@@ -40,21 +63,41 @@ namespace MecAppIN.ViewModels
             AbrirBuscarOrcamentosCommand = new RelayCommand(() =>
                 TelaAtual = new BuscarOrcamentosViewModel());
 
-
-
-
             // Tela inicial
             TelaAtual = new OrdemServicosViewModel();
+
+            // ===============================
+            // TIMER DE DATA / HORA
+            // ===============================
+            AtualizarHora();
+
+            _timer = new DispatcherTimer
+            {
+                Interval = TimeSpan.FromSeconds(1)
+            };
+
+            _timer.Tick += (s, e) => AtualizarHora();
+            _timer.Start();
         }
+
+        // ===============================
+        // EDIÇÃO DE ORÇAMENTO
+        // ===============================
         public void AbrirEdicaoOrcamento(Orcamentos orcamento)
         {
             TelaAtual = new OrcamentosViewModel(orcamento);
         }
 
+        private void AtualizarHora()
+        {
+            DataHoraAtual = DateTime.Now.ToString("dd/MM/yyyy HH:mm");
+        }
 
+        // ===============================
+        // INotifyPropertyChanged
+        // ===============================
         public event PropertyChangedEventHandler PropertyChanged;
-        void OnPropertyChanged([CallerMemberName] string p = null)
-            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(p));
+        private void OnPropertyChanged([CallerMemberName] string prop = null)
+            => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
     }
 }
-
