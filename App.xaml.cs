f7fbb2a522e76cfbf410d4b3decc9e59d1154b4e
CaioSNery <caionery40@gmail.com>
2026-01-29 19:19:50 -0300

    fix : OnStartUp

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,18 +1,23 @@
-﻿using MecAppIN.Data;
-using QuestPDF.Infrastructure;
+﻿using QuestPDF.Infrastructure;
 using System.Windows;
 
 namespace MecAppIN
 {
- public partial class App : Application
-{
-    public App()
+    public partial class App : Application
     {
-        QuestPDF.Settings.License = LicenseType.Community;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            // 1️⃣ Licença do QuestPDF (SEMPRE PRIMEIRO)
+            QuestPDF.Settings.License = LicenseType.Community;
 
-        
+            // 2️⃣ Captura de erro (diagnóstico)
+            DispatcherUnhandledException += (s, ex) =>
+            {
+                MessageBox.Show(ex.Exception.ToString(), "ERRO CAPTURADO");
+                ex.Handled = true;
+            };
+
+            base.OnStartup(e);
+        }
     }
-}
-
-
 }
